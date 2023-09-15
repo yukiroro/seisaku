cf7e2d86da16afc73716947cd0f08a2bdab7aa1f
yukiroro <130203082+yukiroro@users.noreply.github.com>
2023-09-15 22:34:28 +0900

    HttpToHttps

---
+++ app/Http/Middleware/ForceHttpToHttps.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceHttpToHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
+}
